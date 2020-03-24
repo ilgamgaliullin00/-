21a218c595f77d507236b4e357976d93053286c0
ilgamgaliullin00 <55695034+ilgamgaliullin00@users.noreply.github.com>
2020-03-24 21:04:08 +0300

    Add files via upload

---
+++ oobd/Lab2/main/java/Person.java
@@ -0,0 +1,41 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public abstract class Person {
+    String name;
+    Date birthDate;
+    Gender gender;
+
+    public Person() {
+    }
+
+    public Person(String name, Date birthDate, Gender gender) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return this.gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
